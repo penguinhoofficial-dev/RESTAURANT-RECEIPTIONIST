52d301c1a649a247eaa1fcae8e962d3f559e7441
penguinhoofficial-dev <penguinhoofficial@gmail.com>
2026-05-02 19:52:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,3 +31,22 @@ app.post("/check-user", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server running"));
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/book", (req, res) => {
+  console.log("DATA RECEIVED:", req.body);
+
+  res.json({
+    success: true,
+    message: "Booking received"
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(3000, () => console.log("Server running"));
